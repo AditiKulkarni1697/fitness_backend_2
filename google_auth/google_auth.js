d980028469f500fb23680a35a197dd2bd04f1202
AditiSujlegaonkar1697 <aditisujlegaonkar@gmail.com>
2023-10-22 16:59:07 +0530

    google auth

--- google_auth/google_auth.js
+++ google_auth/google_auth.js
@@ -1,4 +1,5 @@
 const passport = require('passport');
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require("dotenv").config()
 
@@ -6,12 +7,16 @@ passport.use(new GoogleStrategy({
     clientID: process.env.google_clinetID,
     clientSecret: process.env.google_clientSecret,
     callbackURL: 'https://fitme-2.onrender.com/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
+}, async(accessToken, refreshToken, profile, done) => {
     // Your verification and user creation logic here
-    fetch('https://www.googleapis.com/oauth2/v2/userinfo')
-    .then((res)=>res.json())
-    .then((res)=>{console.log(res)})
-    .catch((err)=>{console.log(err)})
+    try{
+    const res =  await fetch('https://www.googleapis.com/oauth2/v2/userinfo')
+    res=res.json()
+    console.log(res)
+    }
+    catch(err){
+      console.log(err)
+    }
 
     return done(null, profile);
 }));
