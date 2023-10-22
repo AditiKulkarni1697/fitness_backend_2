3db1230a1198762f68dedc69693789056b525e9e
AditiSujlegaonkar1697 <aditisujlegaonkar@gmail.com>
2023-10-22 19:43:46 +0530

    google oauth

--- index.js
+++ index.js
@@ -52,10 +52,10 @@ app.get('/auth/google/callback',
       const user = await UserModel.findOne({email:req.user})
       if(user){
         console.log(user)
-        res.redirect(`https://65352c794c06bc72bbb35bca--aesthetic-gnome-75ccb6.netlify.app/html/client.html?client=user&data=${user}`)
+        res.redirect(`https://65352dcf7c315e72aa31b9bc--flourishing-baklava-9318a8.netlify.app/html/client.html?client=user&data=${user}`)
       }
       else{
-        res.redirect("https://65352c794c06bc72bbb35bca--aesthetic-gnome-75ccb6.netlify.app/html/signup.html")
+        res.redirect("https://65352dcf7c315e72aa31b9bc--flourishing-baklava-9318a8.netlify.app/html/signup.html")
       }
     }
     catch(err){
